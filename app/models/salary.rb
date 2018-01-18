d02491ee577f6b029448ae867db5325be7340b2a
Tsehau Chao <jodeci@5xruby.tw>
2018-01-18 19:09:57 +0800

    fix naming

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -3,9 +3,23 @@ class Salary < ApplicationRecord
   default_scope { order(start_date: :desc) }
   belongs_to :employee
 
-  def self.by_payroll(employee, period_start, period_end)
-    return if employee.end_date and employee.end_date < period_start
-    find_by("employee_id = ? AND start_date < ?", employee.id, period_end)
+  # TODO: validations
+  # role.regular
+  #   必填 monthly_wage, health_insurance, labor_insurance
+  #   限制 commuting_subsidy: 0, tax_code: 50, hourly_wage: 0
+  #   可填 equipment_subsidy, supervisor_allowance
+  # role.contractor
+  #   必填 monthly_wage
+  #   條件 tax_code: 9a => labor_insurance: 0, health_insurance: 0
+  #   限制 commuting_subsidy: 0, supervisor_allowance: 0, equipment_subsidy: 0, hourly_wage: 0
+  # role.parttime
+  #   必填 hourly_wage
+  #   限制 supervisor_allowance: 0, equipment_subsidy: 0, monthly_wage: 0
+  #   可填 commuting_subsidy
+
+  def self.by_payroll(employee, cycle_start, cycle_end)
+    return if employee.end_date and employee.end_date < cycle_start
+    find_by("employee_id = ? AND start_date < ?", employee.id, cycle_end)
   end
 
   # 一般薪資所得
