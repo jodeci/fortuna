ae676f0294719573fc39f54c97dba8be1b719f16
Tsehau Chao <jodeci@5xruby.tw>
2018-10-29 14:52:32 +0800

    add default timezone

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,5 +14,6 @@ module Fortuna
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Taipei"
   end
 end
