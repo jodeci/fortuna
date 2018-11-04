ef37b057d4d3f24201a81cba63f8e7c84260870d
jodeci <muroijun@gmail.com>
2018-11-05 02:16:46 +0800

    add collection translatable test

---
+++ test/lib/collection_translatable_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require "test_helper"
+require "mocha/minitest"
+
+class CollectionTranslatableTest < ActiveSupport::TestCase
+  def test_translate_given_values
+    subject = DummyObject.new("apple")
+    assert_equal "蘋果", subject.given_fruit
+  end
+
+  def test_methods_not_prefixed_with_given
+    subject = DummyObject.new
+    assert_raises NoMethodError do
+      subject.eat_fruit
+    end
+  end
+
+  def test_respond_to_missing
+    subject = DummyObject.new
+    assert subject.respond_to? :given_anything
+    assert_not subject.respond_to? :eat_fruit
+  end
+
+  class DummyObject
+    include ActiveModel::Model
+    include CollectionTranslatable
+
+    attr_reader :fruit
+
+    FRUIT = { "蘋果": "apple" }.freeze
+
+    def initialize(fruit = nil)
+      @fruit = fruit
+    end
+  end
+end
