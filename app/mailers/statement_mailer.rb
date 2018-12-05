147e6b3ed0abd3d90faa61dcc8dce3bf279b5dd4
Tsehau Chao <jodeci@5xruby.tw>
2018-12-05 10:42:41 +0800

    fix mailer bug

--- app/mailers/statement_mailer.rb
+++ app/mailers/statement_mailer.rb
@@ -2,6 +2,7 @@
 
 class StatementMailer < ApplicationMailer
   def notify_email(statement)
+    builder = StatementPDFBuilder.new(statement)
     attachments[builder.filename] = builder.encrypted_pdf
     mail(
       to: statement.employee.email,
@@ -9,11 +10,5 @@ class StatementMailer < ApplicationMailer
       subject: builder.email_subject
     )
     builder.delete_files
-  end
-
-  private
-
-  def builder
-    @builder ||= StatementPDFBuilder.new(statement)
   end
 end
