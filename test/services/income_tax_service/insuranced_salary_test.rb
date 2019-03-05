06c8f6609fd58d421decf140984f7b536e2e1e34
mindytai175 <mindytai175@gmail.com>
2019-03-05 11:29:40 +0800

    fix for feedback

--- test/services/income_tax_service/insuranced_salary_test.rb
+++ test/services/income_tax_service/insuranced_salary_test.rb
@@ -1,21 +1,22 @@
+# frozen_string_literal: true
 require "test_helper"
 
 module IncomeTaxService
   class InsurancedSalaryTest
-    def has_insuranced_salary_tax
-      subject = prepare_subject(income_tax: 3000)
+    def test_has_insuranced_salary_tax
       assert_equal 3000, IncomeTaxService::InsurancedSalary.call(subject)
     end
 
     private
-    def prepare_subject(income_tax: 0)
+
+    def subject
       build(
         :payroll,
         year: 2018,
         month: 5,
-        salary: build(:salary, monthly_wage: 30000, fixed_income_tax: income_tax),
-        employee: build(:employee),
-      ) { |payroll| create(:extra_entry, income_type: :salary, payroll: payroll) }
+        salary: build(:salary, fixed_income_tax: 3000),
+        employee: build(:employee)
+      )
     end
   end
 end
--- test/services/income_tax_service/regular_employee_test.rb
+++ test/services/income_tax_service/regular_employee_test.rb
@@ -1,25 +1,24 @@
+# frozen_string_literal: true
 require "test_helper"
 
 module IncomeTaxService
   class RegularEmployeeTest < ActiveSupport::TestCase
-
     def test_has_insuranced_salary_and_irregular_income
-      subject = prepare_subject(festival_bonus: 10000, extra_amount: 100000, income_tax: 3000)
       IncomeTaxService::InsurancedSalary.expects(:call).returns(3000)
       IncomeTaxService::IrregularIncome.expects(:call).returns(5500)
-      assert IncomeTaxService::RegularEmployee.call(subject)
+      assert_equal 8500, IncomeTaxService::RegularEmployee.call(subject)
     end
 
     private
-    def prepare_subject(festival_bonus: 0, extra_amount: 0, income_tax: 0)
+
+    def subject
       build(
         :payroll,
         year: 2018,
         month: 5,
-        festival_bonus: festival_bonus,
-        salary: build(:salary, monthly_wage: 30000, fixed_income_tax: income_tax),
-        employee: build(:employee),
-      ) { |payroll| create(:extra_entry, income_type: :salary, amount: extra_amount, payroll: payroll) }
+        salary: build(:salary),
+        employee: build(:employee)
+      )
     end
   end
-end  
+end
